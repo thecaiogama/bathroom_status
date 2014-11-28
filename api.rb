a58641bc554fc75f2309e14a6320c78d57314425
Rodrigo Montanha <rodrigoma@gmail.com>
2014-11-28 16:35:02 -0200

    inclusao da gem do parse.com

--- api.rb
+++ api.rb
@@ -1,13 +1,31 @@
 #!/usr/bin/env ruby
 require 'sinatra'
 require './models/bathroom'
+require 'json'
+require 'parse-ruby-client'
 
 get '/status/:bathroom' do
-  { status => 'LIVRE' }
+	Parse.init :application_id => "kRBzulQgel9HP1SzlLAKQAbFCxLsFbJBmk878gB8",
+           	   :api_key        => "enxZzrPJjZ2V9p6KH83fA7AxsIN8PzpeeiswxG3H",
+               :quiet          => true
+
+    bathroom_p_query = Parse::Query.new("bathroom")
+	bathroom_p_query.eq("name", params[:bathroom])
+	bathroom_p = bathroom_p_query.get
+	puts bathroom_p
+
+	#bathroom = Bathroom.new()
+
+	bathroom_p.to_json
+
+ #{ :status => 'LIVRE' }.to_json
+ #consumir a base
 end
 
 put '/status/' do
   bathroom = Bathroom.new(params[:name], params[:status])
+
+#base
 
   bathroom.as_json
 end
